6028e809593f18e64133851a8d37e14aa3e1107a
Gongsta <gong.steven@hotmail.com>
2019-11-14 18:54:11 -0500

    added code

--- datapreprocessing.py
+++ datapreprocessing.py
@@ -12,14 +12,20 @@ from functions.utils_floodmodel import get_mask_of_basin, add_shifted_variables,
 era5 = xr.open_dataset('../data/reanalysis-era5-pressure-levels_temperature_2000_01.nc')
 
 
-glofas = xr.open_dataset('../data/dataset-cems-glofas-historical-fc9c62e9-1df3-4179-84dd-277f77e620fb/CEMS_ECMWF_dis24_20010107_glofas_v2.1.nc')
-glofas2 = xr.open_dataset('../data/dataset-cems-glofas-historical-fc9c62e9-1df3-4179-84dd-277f77e620fb/CEMS_ECMWF_dis24_20010130_glofas_v2.1.nc')
+glofas = xr.open_dataset('../data/dataset-cems-glofas-historical-fc9c62e9-1df3-4179-84dd-277f77e620fb/CEMS_ECMWF_dis24_20010101_glofas_v2.1.nc')
 
-#for i in file
+for i in range(2,30):
+    if i<10:
+        i = '0' + str(i)
+
+    glofas = glofas.merge(xr.open_dataset('../data/dataset-cems-glofas-historical-fc9c62e9-1df3-4179-84dd-277f77e620fb/CEMS_ECMWF_dis24_200101'+ str(i) + '_glofas_v2.1.nc'))
+
+
+
 
 glofas3 = glofas.merge(glofas2)
 
-glofasplot = glofas3.isel(lat=605, lon=2112)
+glofasplot = glofas3.isel(lat='', lon=2112)
 
 glofasplot.to_dataframe().plot()
 
--- saudiarabia.py
+++ saudiarabia.py
@@ -1,3 +1,5 @@
+#Data extraction
+
 #Saving our API key so that we can use Climate Data Store API to extract data
 UID = '26343'
 API_key = 'b61b1b28-a04b-4cb3-acb1-b48775702011'
@@ -13,14 +15,12 @@ import sys
 sys.path.append('./')
 from functions.data_download import CDS_Dataset
 
-ds = CDS_Dataset(dataset_name='reanalysis-era5-pressure-levels',
+ds = CDS_Dataset(dataset_name='reanalysis-era5-single-levels',
                  save_to_folder='../data/'  # path to where datasets shall be stored
                 )
 
-# define areas of interest (a list of degrees latitude/longitude values for the northern, western, southern and eastern bounds of the area.)
-area_dict = dict(danube=[50, 7, 47, 20],
-                 asia=[55, -140, 0, 35],
-                 usa=[50, -125, 25, -70])
+# define areas of interest (N/W/S/E)
+saudi_arabia = [28,36,17,50]
 
 # define time frame
 year_start = 2005
@@ -31,14 +31,25 @@ month_end = 12
 # define requested variables
 request = dict(product_type='reanalysis',
                format='netcdf',
-               area=area_dict['usa'],
-               variable=['temperature'],
-               pressure_level=['850', '700', '500'])
+               area=saudi_arabia,
+               variable=['convective_precipitation','land_sea_mask','large_scale_precipitation',
+            'runoff','slope_of_sub_gridscale_orography','soil_type',
+            'total_column_water_vapour','volumetric_soil_water_layer_1','volumetric_soil_water_layer_2'],
+               )
 
 #Sending the request
 ds.get(years = [str(y) for y in range(year_start, year_end+1)],
        months = [str(a).zfill(2) for a in range(month_start, month_end+1)],
        request = request,
        N_parallel_requests = 12)
+
+
+
+
+#Model
+era5 = xr.open_dataset('../data/reanalysis-era5-pressure-levels_temperature_2000_01.nc')
+
+glofas = xr.open_dataset('../data/dataset-cems-glofas-historical-fc9c62e9-1df3-4179-84dd-277f77e620fb/CEMS_ECMWF_dis24_20010107_glofas_v2.1.nc')
+
 
 
