4398e38a328c4569f5ab3b9d07cde740b4bdf301
Gongsta <gong.steven@hotmail.com>
2020-02-11 01:13:18 -0500

    ftp download

--- pipeline/data_download:upload/ftp.py
+++ pipeline/data_download:upload/ftp.py
@@ -5,12 +5,12 @@ ftp.cwd("for_StevenGong")
 ftp.dir()
 
 import os
-local_filename = os.path.join("/Users/stevengong/Desktop/flood-prediction/pipeline/data_download:upload/", "file")
+local_filename = os.path.join("/tmp/flood-prediction/pipeline/", "test")
 with open(local_filename, 'wb') as fp:
-     ftp.retrbinary('RETR glofas2.1_reforecast_ref2018_glofas2.1_era5wb_released_dis_points_noobs_2000120300_newobs_stat6122_197901_201712_20190125.nc', fp.write)
+     ftp.retrbinary('RETR glofas2.1_2018_areagrid_for_StevenGong_in_Europe_1998010100.nc', fp.write)
 
 
 import xarray as xr
 
 
-test_file = xr.open_dataset("./pipeline/data_download:upload/file")
+test_file = xr.open_dataset("/tmp/flood-prediction/pipeline/file")
