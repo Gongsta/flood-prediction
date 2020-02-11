6c57943104c79b6d6e2d1552989200f6be2e86ef
Gongsta <gong.steven@hotmail.com>
2020-02-10 23:44:53 -0500

    updated reshape

--- pipeline/data_download:upload/glofas_reshape.py
+++ pipeline/data_download:upload/glofas_reshape.py
@@ -1,12 +1,11 @@
 import sys
 sys.path.append("../")
 
-import xarray as xr
 #Creating a Dask local cluster for parallel computing (making the computations later on much faster)
 from dask.distributed import Client, LocalCluster
 
 import xarray as xr
-glofas_loaded = xr.open_mfdataset("/mnt/bucket/stuarts_files/glofas/*/*.nc", combine="by_coords")
+glofas_loaded = xr.open_mfdataset("/mnt/bucket/stuarts_files/glofas/*/CEMS_ECMWF_dis24_*_glofas_v2.1.nc", combine="by_coords")
 
 glofas = glofas_loaded
 
@@ -15,12 +14,17 @@ mean_discharge = glofas.mean(dim='time')['dis24']
 #Mask to replace normal (i.e. not flooded events with NaN)
 mask = glofas['dis24'] > (mean_discharge * 1.5)
 #Mask to replace values with 0 by NaN
-mask2 = glofas['dis24'] != 0
+#mask2 = glofas['dis24'] != 0
 
-ds2 = glofas['dis24'].where(mask).where(mask2)
+ds2 = glofas['dis24'].where(mask)
 
-for i in range(1999, 2020, 1):
+import shutil
+
+for i in range(2006, 2020, 1):
     period=dict(time=slice(str(i)))
     print(period)
-    print('hello')
-    ds2.loc[period].to_netcdf(('/mnt/bucket/stuarts_files/thresholds_glofas/ver4_reshaped_glofas_'+str(i)+'.nc'))
+    ds2.loc[period].to_netcdf(('./reshaped_glofas_' + str(i) + '.nc'))
+    shutil.move(('./reshaped_glofas_' + str(i) + '.nc'), ('/mnt/bucket/stuarts_files/thresholds_glofas/./reshaped_glofas_' + str(i) + '.nc'))
+
+
+
