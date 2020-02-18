6bc12b42c79d2e598c65662f31fb8504ce6be37e
Gongsta <gong.steven@hotmail.com>
2020-02-18 09:57:36 -0500

    new day new beginning

--- pipeline/data_download:upload/ftp.py
+++ pipeline/data_download:upload/ftp.py
@@ -5,12 +5,34 @@ ftp.cwd("for_StevenGong")
 ftp.dir()
 
 import os
-local_filename = os.path.join("/tmp/flood-prediction/pipeline/", "test")
-with open(local_filename, 'wb') as fp:
-     ftp.retrbinary('RETR glofas2.1_2018_areagrid_for_StevenGong_in_Europe_1998010100.nc', fp.write)
+import tarfile
 
+#Download Europe Files on cloud
 
-import xarray as xr
+# local_filename = os.path.join("/tmp/flood_prediction/pipeline/", "EPS.tar")
+# with open(local_filename, 'wb') as fp:
+#      ftp.retrbinary('RETR glofas2.1_areagrid_for_StevenGongEurope20200207_201306.tar', fp.write)
 
+for i in range(1998,2016):
+     for j in range(1,13):
+          if j < 10:
+               j = '0' + str(j)
 
-test_file = xr.open_dataset("/tmp/flood-prediction/pipeline/file")
+          else:
+               j = str(j)
+
+     local_filename = os.path.join("/Volumes/portableHardDisk/data/", ("europeEPS" + str(i) + j +".tar"))
+     with open(local_filename, 'wb') as fp:
+          ftp.retrbinary(('RETR glofas2.1_areagrid_for_StevenGongEurope20200207_' +str(i) + j +'.tar'), fp.write)
+
+     #Tar extraction in python doesn't work for me
+     # my_tar = tarfile.open(("europeEPS" + str(i) + str(j) +".tar"))
+
+     # my_tar.extractall("/Volumes/portableHardDisk/data/EPS")
+
+#
+#
+# import xarray as xr
+#
+#
+# test_file = xr.open_dataset("/tmp/flood_prediction/pipeline/glofas2.1_2018_areagrid_for_StevenGong_in_Europe_2013062800.nc")
