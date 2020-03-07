690f2f07a906b01aa60b99e7c55086862020de68
Gongsta <gong.steven@hotmail.com>
2020-03-07 00:30:18 -0500

    added elbe files

--- pipeline/data_download/glofas_extraction.py
+++ pipeline/data_download/glofas_extraction.py
@@ -8,6 +8,45 @@ with open(os.path.join(os.path.expanduser('~'), '.cdsapirc2'), 'w') as f:
     f.write(f'key: {UID}:{API_key}')
 
 # #Code for downloading Glofas Data
+import cdsapi
+
+c = cdsapi.Client()
+
+for i in range(1983, 1999):
+
+
+    c.retrieve(
+        'cems-glofas-historical',
+        {
+            'format':'zip',
+            'year':[
+                str(i)
+            ],
+            'variable':'River discharge',
+            'month':[
+                '01', '02', '03', '04', '05', '06'            ],
+            'day':[
+                '01','02','03',
+                '04','05','06',
+                '07','08','09',
+                '10','11','12',
+                '13','14','15',
+                '16','17','18',
+                '19','20','21',
+                '22','23','24',
+                '25','26','27',
+                '28','29','30',
+                '31'
+            ],
+            'dataset':'Consolidated reanalysis',
+            'version':'2.1'
+        },
+        ('/Volumes/portableHardDisk/data/'+str(i) + '.zip'))
+
+#'01','02','03', '04','05','06'
+#'07','08','09','10','11','12'
+
+
 # import cdsapi
 #
 # c = cdsapi.Client()
@@ -15,54 +54,17 @@ with open(os.path.join(os.path.expanduser('~'), '.cdsapirc2'), 'w') as f:
 # c.retrieve(
 #     'cems-glofas-historical',
 #     {
-#         'format':'zip',
-#         'year':[
-#             '2005'
+#         'variable': 'River discharge',
+#         'dataset': 'Consolidated reanalysis',
+#         'version': '2.1',
+#         'year': '2007',
+#         'month': '01',
+#         'day': [
+#             '01', '02', '03',
+#             '04', '05', '06',
+#             '07', '08', '09',
+#             '10', '11', '12',
 #         ],
-#         'variable':'River discharge',
-#         'month':[
-#             '01','02','03', '04','05','06'
-#         ],
-#         'day':[
-#             '01','02','03',
-#             '04','05','06',
-#             '07','08','09',
-#             '10','11','12',
-#             '13','14','15',
-#             '16','17','18',
-#             '19','20','21',
-#             '22','23','24',
-#             '25','26','27',
-#             '28','29','30',
-#             '31'
-#         ],
-#         'dataset':'Consolidated reanalysis',
-#         'version':'2.1'
+#         'format': 'zip',
 #     },
-#     '2005.zip')
-
-#'01','02','03', '04','05','06'
-#'07','08','09','10','11','12'
-
-
-import cdsapi
-
-c = cdsapi.Client()
-
-c.retrieve(
-    'cems-glofas-historical',
-    {
-        'variable': 'River discharge',
-        'dataset': 'Consolidated reanalysis',
-        'version': '2.1',
-        'year': '2007',
-        'month': '01',
-        'day': [
-            '01', '02', '03',
-            '04', '05', '06',
-            '07', '08', '09',
-            '10', '11', '12',
-        ],
-        'format': 'zip',
-    },
-    'glofas.zip')
+#     'glofas.zip')
